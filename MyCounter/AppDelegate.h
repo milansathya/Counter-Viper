6f3c87226c7303a0b50559fd6fbbc74ed1f045f0
Sathyanarayan Chandrashekar <schandrashekar@paypal.com>
2017-12-16 18:20:19 -0800

    Initial Commit

---
+++ MyCounter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCounter
+//
+//  Created by Chandrashekar, Sathyanarayan on 12/16/17.
+//  Copyright © 2017 Chandrashekar, Sathyanarayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCounter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCounter
+//
+//  Created by Chandrashekar, Sathyanarayan on 12/16/17.
+//  Copyright © 2017 Chandrashekar, Sathyanarayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
